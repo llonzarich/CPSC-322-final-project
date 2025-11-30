e56a31a11796134608a919e40790ab80c1e49e53
llonzarich <llonzarich@zagmail.gonzaga.edu>
2025-11-30 12:04:54 -0800

    progress on RF clf fit()

--- test_myrandomforest.py
+++ test_myrandomforest.py
@@ -3,7 +3,9 @@ from mysklearn.myclassifiers import MyRandomForestClassifier
 from mysklearn import myutils
 
 def test_generate_test_split():
-    
+    '''
+        Purpose: test function for generating test set split.
+    '''
     # interview dataset
     X_train_interview = [
             ["Senior", "Java", "no", "no"], # False
@@ -44,3 +46,40 @@ def test_generate_test_split():
     testing_y_freq = myutils.get_frequency(myForest.y_test)
     for key in testing_y_freq:
         assert round(testing_y_freq[key]/len(myForest.y_test)) == round(actual_y_freq[key]/len(y_train_interview))
+
+
+def test_random_forest_classifier_fit():
+    '''
+        Purpose: test function for random forest classifier model training function.
+    '''
+    # interview dataset
+    X_train_interview = [
+            ["Senior", "Java", "no", "no"], # False
+            ["Senior", "Java", "no", "yes"], # False
+            ["Mid", "Python", "no", "no"], # True
+            ["Junior", "Python", "no", "no"], # True
+            ["Junior", "R", "yes", "no"], # True
+            ["Junior", "R", "yes", "yes"], # False
+            ["Mid", "R", "yes", "yes"], # True
+            ["Senior", "Python", "no", "no"], # False
+            ["Senior", "R", "yes", "no"], # True
+            ["Junior", "Python", "yes", "no"], # True
+            ["Senior", "Python", "yes", "yes"], # True
+            ["Mid", "Python", "no", "yes"], # True
+            ["Mid", "Java", "yes", "no"], # True
+            ["Junior", "Python", "no", "yes"] # False
+        ]
+    y_train_interview = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
+    
+    myForest = MyRandomForestClassifier(N=20, M=7, F=2)
+    myForest.fit(X_train_interview, y_train_interview)
+
+    # retrieve the forest from the fit() method.
+    forest = myForest.trees
+
+    assert forest is not None
+    assert forest == forest_interview
+
+
+def test_random_forest_classifier_predict():
+    pass
