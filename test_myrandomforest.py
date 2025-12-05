b3601762f09eda6a764137638ab711e47440146d
KatieP1 <Katiepark1995@yahoo.com>
2025-12-05 00:47:41 -0800

    added more graphs

--- test_myrandomforest.py
+++ test_myrandomforest.py
@@ -1,4 +1,4 @@
-from mysklearn.myclassifiers import MyDecisionTreeClassifier, MyRandomForestClassifier
+from mysklearn.myclassifiers import MyRandomForestClassifier, MyDecisionTreeClassifier
 from mysklearn import myutils
 
 def test_generate_test_split():
@@ -25,26 +25,31 @@ def test_generate_test_split():
     y_train_interview = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
     
     myForest = MyRandomForestClassifier()
-    myForest.generate_test_set(X_train_interview, y_train_interview)
+    X_train, X_test, y_train, y_test = myForest.generate_test_set(X_train_interview, y_train_interview)
 
     # check to see that the sizes of both the training and test sets combined are 
     # the same size as the original dataset (same number of rows)
-    assert len(myForest.X_test) + len(myForest.X_train) == 14
+    assert len(X_test) + len(X_train) == 14
 
     # check to see that a row in the testing set is not in the training set
     # (no duplicates between training and testing sets), and vice versa
-    for row in myForest.X_test:
-        assert row not in myForest.X_train
+    for row in X_test:
+        assert row not in X_train
 
-    for row in myForest.X_train:
-        assert row not in myForest.X_test
+    for row in X_train:
+        assert row not in X_test
 
     # check to see the test set has the same class label distribution as 
     # the original dataset
     actual_y_freq = myutils.get_frequency(y_train_interview)
-    testing_y_freq = myutils.get_frequency(myForest.y_test)
+    testing_y_freq = myutils.get_frequency(y_test)
     for key in testing_y_freq:
-        assert round(testing_y_freq[key]/len(myForest.y_test)) == round(actual_y_freq[key]/len(y_train_interview))
+        assert round(testing_y_freq[key]/len(y_test)) == round(actual_y_freq[key]/len(y_train_interview))
+
+    # check to see if training and test sets are both equal length
+    assert len(X_train) == len(y_train)
+
+    assert len(X_test) == len(y_test)
 
 
 def test_random_forest_classifier_fit():
@@ -71,7 +76,8 @@ def test_random_forest_classifier_fit():
     y_train_interview = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
 
     myForest = MyRandomForestClassifier(N=20, M=7, F=2)
-    myForest.fit(X_train_interview, y_train_interview, random_state=8)
+    X_train, X_test, y_train, y_test = myForest.generate_test_set(X_train_interview, y_train_interview)
+    myForest.fit(X_train, y_train)
 
     # retrieve the forest from the fit() method.
     forest = myForest.trees
